8271e9caa505c0ac8aa86f035eb55dd9b88c778e
Derrick W. Turk <dwt@terminusdatascience.com>
2014-06-13 10:50:23 -0500

    Sweet tuple-forwarding tools!

---
+++ tuple_tools.hpp
@@ -0,0 +1,87 @@
+#ifndef TUPLE_TOOLS_HPP
+
+#include <cstddef>
+#include <type_traits>
+#include <tuple>
+#include <utility>
+
+namespace tuple {
+
+namespace detail {
+
+template<std::size_t N>
+struct apply_impl {
+    template<class F, class Tuple, class... TParams>
+    static auto apply(F&& fn, Tuple&& t, TParams&&... args)
+      ->  decltype(
+              apply_impl<N - 1>::apply(
+                  std::forward<F>(fn), std::forward<Tuple>(t),
+                  std::get<N - 1>(std::forward<Tuple>(t)),
+                  std::forward<TParams>(args)...
+          ))
+    {
+        return apply_impl<N - 1>::apply(
+                std::forward<F>(fn), std::forward<Tuple>(t),
+                std::get<N - 1>(std::forward<Tuple>(t)),
+                std::forward<TParams>(args)...
+                );
+    }
+};
+
+template<>
+struct apply_impl<0> {
+    template<class F, class Tuple, class... TParams>
+    static auto apply(F&& fn, Tuple&& t, TParams&&... args)
+      -> decltype(std::forward<F>(fn)(std::forward<TParams>(args)...))
+    {
+        return std::forward<F>(fn)(std::forward<TParams>(args)...);
+    }
+};
+
+template<std::size_t N>
+struct construct_impl {
+    template<class T, class Tuple, class... TParams>
+    static T construct(Tuple&& t, TParams&&... args)
+    {
+        return construct_impl<N - 1>::template construct<T>(
+                std::forward<Tuple>(t),
+                std::get<N - 1>(std::forward<Tuple>(t)),
+                std::forward<TParams>(args)...
+                );
+    }
+};
+
+template<>
+struct construct_impl<0> {
+    template<class T, class Tuple, class... TParams>
+    static T construct(Tuple&& t, TParams&&... args)
+    {
+        return T(std::forward<TParams>(args)...);
+    }
+};
+
+}
+
+template<class F, class Tuple>
+auto apply(F&& fn, Tuple&& t)
+  -> decltype(detail::apply_impl<
+          std::tuple_size<typename std::decay<Tuple>::type>::value
+        >::apply(std::forward<F>(fn), std::forward<Tuple>(t)))
+{
+    return detail::apply_impl<
+        std::tuple_size<typename std::decay<Tuple>::type>::value
+      >::apply(std::forward<F>(fn), std::forward<Tuple>(t));
+}
+
+template<class T, class Tuple>
+T construct(Tuple&& t)
+{
+    return detail::construct_impl<
+        std::tuple_size<typename std::decay<Tuple>::type>::value
+      >::template construct<T>(std::forward<Tuple>(t));
+}
+
+}
+
+#define TUPLE_TOOLS_HPP
+#endif

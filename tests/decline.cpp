cb23062888e264701fb40cbeaeaf5fbfd6f2214a
Derrick W. Turk <dwt@terminusdatascience.com>
2014-07-25 15:02:20 -0500

    Test harmonic case.

--- tests/decline.cpp
+++ tests/decline.cpp
@@ -56,4 +56,24 @@ BOOST_AUTO_TEST_CASE( conversions )
 
     BOOST_CHECK_EQUAL(dsec,
             (convert_decline<secant_effective, secant_effective>(dsec, b)));
+
+    // check potentially degenerate cases
+    b = 1.0;
+
+    BOOST_CHECK_CLOSE(dtan,
+            (convert_decline<secant_effective, tangent_effective>(
+                convert_decline<tangent_effective, secant_effective>(
+                    dtan, b), b)),
+            tolerance_pct);
+
+    BOOST_CHECK_CLOSE(dsec,
+            (convert_decline<tangent_effective, secant_effective>(
+                convert_decline<secant_effective, tangent_effective>(
+                    dsec, b), b)),
+            tolerance_pct);
+
+    BOOST_CHECK_CLOSE(dsec,
+            (convert_decline<nominal, secant_effective>(
+                decline<secant_effective>(dsec, b), b)),
+            tolerance_pct);
 }

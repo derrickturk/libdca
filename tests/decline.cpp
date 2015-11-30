e712e4c456832cb1518be5984c76b20b2bb16119
derrickturk <dwt@terminusdatascience.com>
2015-11-29 19:52:12 -0600

    Test EUR/time-to-rate/cum-at-time match for random declines.

--- tests/decline.cpp
+++ tests/decline.cpp
@@ -1,8 +1,14 @@
 #include "dca/decline.hpp"
+#include "dca/exponential.hpp"
+#include "dca/hyperbolic.hpp"
+#include "dca/hyptoexp.hpp"
 
 #define BOOST_TEST_MODULE decline
 #include <boost/test/unit_test.hpp>
 #include <boost/test/floating_point_comparison.hpp>
+
+#include <random>
+#include <cmath>
 
 const double tolerance_pct = 1e-2;
 
@@ -172,9 +178,58 @@ BOOST_AUTO_TEST_CASE( eur )
     dca::arps_exponential decl(1000,
             dca::decline<dca::tangent_effective>(0.65));
     double time, ultimate = dca::eur(decl, 1.0, 30.0, &time);
-    BOOST_CHECK_EQUAL(decl.cumulative(time), ultimate);
+    BOOST_CHECK_CLOSE(decl.cumulative(time), ultimate, tolerance_pct);
     BOOST_CHECK_CLOSE(time, (dca::time_to_cumulative(decl, ultimate)),
             tolerance_pct);
     BOOST_CHECK_CLOSE(time, (dca::time_to_rate(decl, decl.rate(time))),
             tolerance_pct);
+}
+
+BOOST_AUTO_TEST_CASE( random_eur )
+{
+    const int n_test = 1000;
+    std::mt19937 rng;
+
+    std::uniform_real_distribution<> qi_log_dist(0.0, 7.0);
+    std::uniform_real_distribution<> Di_tangent_dist(0.0, 1.0);
+    for (int i = 0; i < n_test; ++i) {
+        dca::arps_exponential decl(std::pow(qi_log_dist(rng), 10.0),
+                dca::decline<dca::tangent_effective>(Di_tangent_dist(rng)));
+        double time, ultimate = dca::eur(decl, 1.0, 30.0, &time);
+        BOOST_CHECK_CLOSE(decl.cumulative(time), ultimate, tolerance_pct);
+        BOOST_CHECK_CLOSE(time, (dca::time_to_cumulative(decl, ultimate)),
+                tolerance_pct);
+        BOOST_CHECK_CLOSE(time, (dca::time_to_rate(decl, decl.rate(time))),
+                tolerance_pct);
+    }
+
+    std::uniform_real_distribution<> b_dist(0.0, 2.5);
+    for (int i = 0; i < n_test; ++i) {
+        dca::arps_hyperbolic decl(std::pow(qi_log_dist(rng), 10.0),
+                dca::decline<dca::tangent_effective>(Di_tangent_dist(rng)),
+                b_dist(rng));
+        double time, ultimate = dca::eur(decl, 1.0, 30.0, &time);
+        BOOST_CHECK_CLOSE(decl.cumulative(time), ultimate, tolerance_pct);
+        BOOST_CHECK_CLOSE(time, (dca::time_to_cumulative(decl, ultimate)),
+                tolerance_pct);
+        BOOST_CHECK_CLOSE(time, (dca::time_to_rate(decl, decl.rate(time))),
+                tolerance_pct);
+    }
+
+    // std::uniform_real_distribution<> Df_tangent_dist(0.0, 2.5);
+    for (int i = 0; i < n_test; ++i) {
+        auto Di_tangent = Di_tangent_dist(rng);
+        std::uniform_real_distribution<> Df_tangent_dist(0.0, Di_tangent);
+        dca::arps_hyperbolic_to_exponential decl(
+                std::pow(qi_log_dist(rng), 10.0),
+                dca::decline<dca::tangent_effective>(Di_tangent),
+                b_dist(rng),
+                dca::decline<dca::tangent_effective>(Df_tangent_dist(rng)));
+        double time, ultimate = dca::eur(decl, 1.0, 30.0, &time);
+        BOOST_CHECK_CLOSE(decl.cumulative(time), ultimate, tolerance_pct);
+        BOOST_CHECK_CLOSE(time, (dca::time_to_cumulative(decl, ultimate)),
+                tolerance_pct);
+        BOOST_CHECK_CLOSE(time, (dca::time_to_rate(decl, decl.rate(time))),
+                tolerance_pct);
+    }
 }
